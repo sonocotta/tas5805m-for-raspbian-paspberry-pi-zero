7bda55bf991aeefee7bdbf9a63412f8b7fdb3cd6
Andriy Malyshenko <andrey.malyshenko@gmail.com>
2024-06-07 10:11:27 +0200

    Update tas5805m.c (#4)

--- tas5805m.c
+++ tas5805m.c
@@ -402,7 +402,7 @@ MODULE_DEVICE_TABLE(of, tas5805m_of_match);
 #endif
 
 static struct i2c_driver tas5805m_i2c_driver = {
-	.probe_new	= tas5805m_i2c_probe,
+	.probe		= tas5805m_i2c_probe,
 	.remove		= tas5805m_i2c_remove,
 	.id_table	= tas5805m_i2c_id,
 	.driver		= {
